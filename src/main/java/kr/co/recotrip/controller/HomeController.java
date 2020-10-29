617ebc2da533be06b28dbb2777f076886167088f
DESKTOP-PLRHEFA\화댕 <ghkwls0607@naver.com>
2020-10-29 13:50:43 +0900

    [10.29] 화댕 컨트롤러 log4j

--- src/main/java/kr/co/recotrip/controller/HomeController.java
+++ src/main/java/kr/co/recotrip/controller/HomeController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Handles requests for the application home page.
@@ -23,17 +24,14 @@ public class HomeController {
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
+	public String home(Model model) {
+		return "detail";
+	}
+	
+	@RequestMapping(value = "/reviewwrite", method = RequestMethod.GET)
+	public String reviewwrite(@RequestParam String review) {
+		logger.info("후기 작성 : "+review);
+		return "detail";
 	}
 	
 }
